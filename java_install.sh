16cc0d406888cfbcf9eeb9e256b025d7586ff763
jacob <spuntik1@protonmail.com>
2016-07-21 18:00:43 -0400

    v0.6.1
    Update

--- java_install.sh
+++ java_install.sh
@@ -6,28 +6,28 @@ sudo apt-get purge openjdk-\*
 
 mkdir -p ${USER_HOME}/java
 
-sudo cp -r ${USER_HOME}/Downloads/jdk-8u91-linux-x64.tar.gz ${USER_HOME}/java
+sudo cp -r ${USER_HOME}/Downloads/jdk-8u101-linux-x64.tar.gz ${USER_HOME}/java
 
 cd ${USER_HOME}/java
 
-sudo tar xvzf jdk-8u91-linux-x64.tar.gz
+sudo tar xvzf jdk-8u101-linux-x64.tar.gz
 
 cd
 
-echo "JAVA_HOME=${USER_HOME}/java/jdk1.8.0_91" >> sudo /etc/profile
-echo "JRE_HOME=${USER_HOME}java/jre1.8.0_91" >> sudo /etc/profile
+echo "JAVA_HOME=${USER_HOME}/java/jdk1.8.0_101" >> sudo /etc/profile
+echo "JRE_HOME=${USER_HOME}java/jre1.8.0_101" >> sudo /etc/profile
 echo "PATH=$PATH:$JRE_HOME/bin:$JAVA_HOME/bin" >> sudo /etc/profile
 echo "export JAVA_HOME" >> sudo /etc/profile
 echo "export JRE_HOME" >> sudo /etc/profile
 echo "export PATH" >> sudo /etc/profile
 
-sudo update-alternatives --install "/usr/bin/java" "java" "${USER_HOME}/java/jdk1.8.0_91/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "${USER_HOME}/java/jdk1.8.0_91/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "${USER_HOME}/java/jdk1.8.0_91/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/java" "java" "${USER_HOME}/java/jdk1.8.0_101/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "${USER_HOME}/java/jdk1.8.0_101/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "${USER_HOME}/java/jdk1.8.0_101/bin/javaws" 1
 
-sudo update-alternatives --set java ${USER_HOME}/java/jdk1.8.0_91/bin/java
-sudo update-alternatives --set javac ${USER_HOME}/java/jdk1.8.0_91/bin/javac 
-sudo update-alternatives --set javaws ${USER_HOME}/java/jdk1.8.0_91/bin/javaws
+sudo update-alternatives --set java ${USER_HOME}/java/jdk1.8.0_101/bin/java
+sudo update-alternatives --set javac ${USER_HOME}/java/jdk1.8.0_101/bin/javac 
+sudo update-alternatives --set javaws ${USER_HOME}/java/jdk1.8.0_101/bin/javaws
 
 source /etc/profile
 
